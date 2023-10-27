545814bad9ed66fe418e9c112d9b36d4910987d4
Mukho <mukho@kakao.com>
2023-10-27 12:50:58 +0900

    Feat: User 관련 기능

---
+++ src/main/java/com/mukho/underworld_line_plus_be/dto/user/LoginUserDto.java
@@ -0,0 +1,19 @@
+package com.mukho.underworld_line_plus_be.dto.user;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class LoginUserDto {
+
+	int userId;
+	String id;
+	String name;
+
+}
