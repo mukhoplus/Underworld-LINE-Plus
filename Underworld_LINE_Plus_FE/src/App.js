798bddf4165c7b53f7f3559adde19d5e63395b15
Mukho <mukho@kakao.com>
2023-10-27 21:03:59 +0900

    Feat: 세션 불러오기

--- Underworld_LINE_Plus_FE/src/App.js
+++ Underworld_LINE_Plus_FE/src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
+import axios from "axios";
+import { setUserId } from "./redux/action";
 import Hello from "./pages/hello/Hello";
 import Main from "./pages/main/Main";
 
@@ -12,7 +14,26 @@ const mapStateToProps = (state) => {
   };
 };
 
-const App = ({ userId, roomList, chatId, chatList }) => {
+const mapDispatchToProps = (dispatch) => ({
+  setUserId: (id) => dispatch(setUserId(id)),
+});
+
+const App = ({ userId, setUserId, roomList, chatId, chatList }) => {
+  const [isSession, setIsSession] = useState(false);
+
+  useEffect(() => {
+    const getSessionUserId = async () => {
+      await axios.get("/user/session").then((response) => {
+        const sessionUserId = response.data;
+        setUserId(sessionUserId);
+        setIsSession(true);
+      });
+    };
+
+    getSessionUserId();
+  }, [setUserId]);
+
+  if (!isSession) return;
   return (
     <>
       {userId === 0 ? (
@@ -29,4 +50,4 @@ const App = ({ userId, roomList, chatId, chatList }) => {
   );
 };
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- Underworld_LINE_Plus_FE/src/index.js
+++ Underworld_LINE_Plus_FE/src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import AxiosProvider from "axios";
 import store from "./redux/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
+
+AxiosProvider.defaults.baseURL = "http://localhost:8080/api/v1";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
