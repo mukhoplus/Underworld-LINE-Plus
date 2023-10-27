41974c960b19a8c98216511ab5c8dbc4b0834523
Mukho <mukho@kakao.com>
2023-10-27 12:51:27 +0900

    Feat: Room 관련 기능

---
+++ src/main/java/com/mukho/underworld_line_plus_be/dto/room/RoomDto.java
@@ -0,0 +1,21 @@
+package com.mukho.underworld_line_plus_be.dto.room;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class RoomDto {
+
+	int roomId;
+	String roomName;
+	String lastMessage;
+	LocalDateTime updatedAt;
+	int notReadCount;
+
+}
