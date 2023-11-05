6dd065af30ec015e5dd95d2e66d2005d9e32d5c8
Mukho <mukho@kakao.com>
2023-11-05 14:30:03 +0900

    Fix: JsonFormat 어노테이션 추가 - JSON -> String 파싱 시 날짜 타입이 string format에서 array로 바뀌는 버그 수정

--- Underworld_LINE_Plus_BE/src/main/java/com/mukho/underworld_line_plus_be/dto/chat/ChatDto.java
+++ Underworld_LINE_Plus_BE/src/main/java/com/mukho/underworld_line_plus_be/dto/chat/ChatDto.java
@@ -3,6 +3,7 @@ package com.mukho.underworld_line_plus_be.dto.chat;
 import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -15,6 +16,7 @@ public class ChatDto {
 	int sendUserId;
 	String message;
 	int notRead;
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "KST")
 	LocalDateTime sendAt;
 
 }
--- Underworld_LINE_Plus_BE/src/main/java/com/mukho/underworld_line_plus_be/dto/room/Room.java
+++ Underworld_LINE_Plus_BE/src/main/java/com/mukho/underworld_line_plus_be/dto/room/Room.java
@@ -3,6 +3,7 @@ package com.mukho.underworld_line_plus_be.dto.room;
 import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -15,6 +16,7 @@ public class Room {
 	int roomId;
 	String identifier;
 	String lastMessage;
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "KST")
 	LocalDateTime updatedAt;
 
 }
