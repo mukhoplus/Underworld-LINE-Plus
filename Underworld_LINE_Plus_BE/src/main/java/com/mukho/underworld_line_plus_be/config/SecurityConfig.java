eed1a94dde8fdc7e5552ee247ae54049fef9e0ab
Mukho <mukho@kakao.com>
2023-11-11 22:44:15 +0900

    Feat: API 호출 보안

--- Underworld_LINE_Plus_BE/src/main/java/com/mukho/underworld_line_plus_be/config/SecurityConfig.java
+++ Underworld_LINE_Plus_BE/src/main/java/com/mukho/underworld_line_plus_be/config/SecurityConfig.java
@@ -2,14 +2,66 @@ package com.mukho.underworld_line_plus_be.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.mukho.underworld_line_plus_be.filter.HeaderValidationFilter;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+@SuppressWarnings("deprecation")
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.cors().and()
+			.csrf().disable()
+			.addFilterAfter(new HeaderValidationFilter(), BasicAuthenticationFilter.class)
+			.authorizeRequests()
+			.antMatchers("/**").permitAll()
+			.anyRequest().authenticated();
+
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.addAllowedOrigin("http://localhost");
+		configuration.addAllowedMethod("*");
+		configuration.addAllowedHeader("*");
+		configuration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+
+		return source;
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.builder()
+			.username("user")
+			.password("")
+			.roles("USER")
+			.build();
+
+		return new InMemoryUserDetailsManager(user);
 	}
 
 }
